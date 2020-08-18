29adeb8931ccd78d53eb7e44b58fddc4ff01dedd
NiravSurajlal <niravsurajlal@gmail.com>
2020-08-18 13:00:56 +0200

    Added amount to DB and doing give function.

--- Program/API/flaskr/blog.py
+++ Program/API/flaskr/blog.py
@@ -12,7 +12,7 @@ def index():
 
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, title, body, created, author_id, username, amount'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
@@ -27,19 +27,28 @@ def create():
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
+        amount = request.form['amount']
         error = None
 
         if not title:
             error = 'Title is required.'
+
+        if not amount:
+            error = 'Amount is required.'
+
+        try:
+            float(amount)
+        except ValueError:
+            error = 'Invalid amount.'
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO post (title, body, author_id)'
-                ' VALUES (?, ?, ?)',
-                (title, body, g.user['id'])
+                'INSERT INTO post (title, body, amount, author_id)'
+                ' VALUES (?, ?, ?, ?)',
+                (title, body, amount, g.user['id'])
             )
             # commit to DB            
             db.commit()
@@ -100,16 +109,32 @@ def update(id):
 @login_required
 def delete(id):
     """ Allows deletion of post. Just a button in 'UPDATE'. """
-        
+
     get_post(id)
     db = get_db()
     db.execute('DELETE FROM post WHERE id = ?', (id,))
     db.commit()
     return redirect(url_for('blog.index'))
 
-# @bp.route('/<int:id>/payment', methods=('GET', 'POST'))
-# @login_required
-# def give_loan(id):
-#     post = get_post(id)
+
+# incomplete
+@bp.route('/<int:id>/payment', methods=('GET', 'POST'))
+@login_required
+def payment(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        amount = request.form['amount']
+        error = None    
+
+        if not amount:
+            error = 'Amount is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            return redirect(url_for('blog.index'))      
+
+    return render_template('blog/payment.html', post=post)               
 
     
